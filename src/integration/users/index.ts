a55ed3754e7ad73e1468eafe086b26d88b346e5b
vitalino silva <vito@helu.io>
2023-03-13 16:37:23 +0000

    updated

--- src/integration/users/index.ts
+++ src/integration/users/index.ts
@@ -1,5 +1,5 @@
 import {UsersService} from "./users.service";
-//import {UsersApiClientAdapter} from "./adapters/users-api-client.adapter";
-import {UsersMockDataAdapter} from "./adapters/users-mock-data.adapter";
+import {UsersApiClientAdapter} from "./adapters/users-api-client.adapter";
+//import {UsersMockDataAdapter} from "./adapters/users-mock-data.adapter";
 
-export default UsersService(UsersMockDataAdapter());
+export default UsersService(UsersApiClientAdapter());
